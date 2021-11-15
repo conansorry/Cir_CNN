a27ebbee89f53fe85f211a3461b13e3fbc7b7146
conansorry <conan_jiang@hotmail.com>
2021-11-15 12:21:24 +0800

    large nw code

--- Gloable_Var.py
+++ Gloable_Var.py
@@ -1,5 +1,8 @@
-data_p = "D:\Dropbox\Python Program\Forward Interpretor\\test\\"
+# data_p = "D:\Jiangy\JY_Python\Fast_forward_generator\Save\\"
 
 
+
+
+data_p = "D:\Jiangy\GAN_Training_Data\Save\\"
 n_pix = 32
-N_pack = 100
+N_pack = 1000
